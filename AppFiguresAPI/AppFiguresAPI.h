e4bc72ed330599090f9bf711cbd1705dc756dbab
Kyle Hickinson <kyle.hickinson@gmail.com>
2011-11-29 00:40:13 -0500

    Initial Commit

---
+++ AppFiguresAPI/AppFiguresAPI.h
@@ -0,0 +1,13 @@
+//
+//  AppFiguresAPI.h
+//  AppFiguresAPI
+//
+//  Created by Kyle Hickinson on 11-11-29.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AppFiguresAPI : NSObject
+
+@end
---
+++ AppFiguresAPI/AppFiguresAPI.m
@@ -0,0 +1,23 @@
+//
+//  AppFiguresAPI.m
+//  AppFiguresAPI
+//
+//  Created by Kyle Hickinson on 11-11-29.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "AppFiguresAPI.h"
+
+@implementation AppFiguresAPI
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
